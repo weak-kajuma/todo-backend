b10eb4c8a1c9022b252ec7efad36df1376bf69d7
weak-kajuma <kazuma826826@gmail.com>
2022-09-12 16:48:06 +0900

    bugs check

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from api.models.task import Base
 from api.models.user import Base
 
-DB_URL = "mysql+pymysql://root@db:3306/todo-backend?charset=utf8"
+DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
 engine = create_engine(DB_URL, echo=True)
 
 
